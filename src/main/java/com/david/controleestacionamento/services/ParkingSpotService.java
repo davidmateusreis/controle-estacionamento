cb50d543229f5a51149744f6f56a4d02b24f8798
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-06-06 12:55:52 -0300

    Adicionei classes e anotações

---
+++ src/main/java/com/david/controleestacionamento/services/ParkingSpotService.java
@@ -0,0 +1,47 @@
+package com.david.controleestacionamento.services;
+
+import com.david.controleestacionamento.models.ParkingSpotModel;
+import com.david.controleestacionamento.repositories.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public  ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) { //método que salva os dados convertidos
+
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+
+    public List<ParkingSpotModel> findAll() {
+        return parkingSpotRepository.findAll();
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
+}
